8a264dbc275e323b8a458dab7becb28612765f97
asidhu0 <asidhu359@gmail.com>
2024-07-19 21:51:33 +0000

    Add all files

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.9
+WORKDIR /app
+COPY step1.py /app
+COPY step2.py /app
+COPY step3.py /app
---
+++ step1.py
@@ -0,0 +1,19 @@
+import os
+
+def detemineDatasetsToRun():
+    print("In detemine datasets to run try 16")
+    for root, dirs, files in os.walk('.'):
+        print(f"Root: {root}")
+        for dir in dirs:
+            print(f"Directory: {os.path.join(root, dir)}")
+        for file in files:
+            print(f"File: {os.path.join(root, file)}")
+    file_path = '/mnt/vol/datasets_to_run.txt'
+    with open(file_path, 'r') as file_list:
+        file_ids = [line.strip() for line in file_list]
+
+    print(f"Need to process these datasets: {file_ids}")
+
+detemineDatasetsToRun()
+
+print("Finished step 1")
