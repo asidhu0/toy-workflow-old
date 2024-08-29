e0afde31cb458fd1f501eeaec2dc0a4510a92201
asidhu0 <asidhu359@gmail.com>
2024-08-29 23:55:01 +0000

    Check type and value of input

--- step2.py
+++ step2.py
@@ -43,6 +43,8 @@ import json
 def doubleNumbers(read_data):
     try:
         input_data = json.loads(sys.argv[1])
+        print(input_data)
+        print(type(input_data))
         input_number = int(input_data)
         result = input_number * 2
         print(result)
