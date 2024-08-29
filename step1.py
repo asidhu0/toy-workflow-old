ca347e7e2426d7e975f741ac5444744f1db46637
asidhu0 <asidhu359@gmail.com>
2024-08-29 23:52:23 +0000

    Removed extra comments

--- step1.py
+++ step1.py
@@ -23,12 +23,12 @@ import json
 import sys
 
 def detemineDatasetsToRun():
-    for root, dirs, files in os.walk('.'):
-        print(f"Root: {root}")
-        for dir in dirs:
-            print(f"Directory: {os.path.join(root, dir)}")
-        for file in files:
-            print(f"File: {os.path.join(root, file)}")
+    # for root, dirs, files in os.walk('.'):
+    #     print(f"Root: {root}")
+    #     for dir in dirs:
+    #         print(f"Directory: {os.path.join(root, dir)}")
+    #     for file in files:
+    #         print(f"File: {os.path.join(root, file)}")
     file_path = '/mnt/vol/datasets_to_run.txt'
     with open(file_path, 'r') as file_list:
         json.dump([line.strip() for line in file_list], sys.stdout)
