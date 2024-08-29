cfae382333bb4ae2183aa83773df72f9f09713ee
asidhu0 <asidhu359@gmail.com>
2024-08-29 23:31:44 +0000

    Check files in docker container

--- step1.py
+++ step1.py
@@ -23,6 +23,12 @@ import json
 import sys
 
 def detemineDatasetsToRun():
+    for root, dirs, files in os.walk('.'):
+        print(f"Root: {root}")
+        for dir in dirs:
+            print(f"Directory: {os.path.join(root, dir)}")
+        for file in files:
+            print(f"File: {os.path.join(root, file)}")
     file_path = '/mnt/vol/datasets_to_run.txt'
     file_path = './datasets_to_run.txt'
     with open(file_path, 'r') as file_list:
