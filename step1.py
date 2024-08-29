3cd3db39fc9dafea4d09a519278c8f533d42a7d1
asidhu0 <asidhu359@gmail.com>
2024-08-29 23:21:37 +0000

    Using json in scripts

--- step1.py
+++ step1.py
@@ -1,19 +1,33 @@
+# import os
+
+# def detemineDatasetsToRun():
+#     print("In detemine datasets to run try 21")
+#     for root, dirs, files in os.walk('.'):
+#         print(f"Root: {root}")
+#         for dir in dirs:
+#             print(f"Directory: {os.path.join(root, dir)}")
+#         for file in files:
+#             print(f"File: {os.path.join(root, file)}")
+#     file_path = '/mnt/vol/datasets_to_run.txt'
+#     with open(file_path, 'r') as file_list:
+#         file_ids = [line.strip() for line in file_list]
+
+#     print(f"Need to process these datasets: {file_ids}")
+
+# detemineDatasetsToRun()
+
+# print("Finished step 1")
+
 import os
+import json
+import sys
 
 def detemineDatasetsToRun():
-    print("In detemine datasets to run try 21")
-    for root, dirs, files in os.walk('.'):
-        print(f"Root: {root}")
-        for dir in dirs:
-            print(f"Directory: {os.path.join(root, dir)}")
-        for file in files:
-            print(f"File: {os.path.join(root, file)}")
     file_path = '/mnt/vol/datasets_to_run.txt'
+    file_path = './datasets_to_run.txt'
     with open(file_path, 'r') as file_list:
-        file_ids = [line.strip() for line in file_list]
-
-    print(f"Need to process these datasets: {file_ids}")
+        json.dump([line.strip() for line in file_list], sys.stdout)
 
 detemineDatasetsToRun()
 
-print("Finished step 1")
+# print("Finished step 1")
--- step2.py
+++ step2.py
@@ -1,35 +1,53 @@
+# import os
+# import sys
+
+# def doubleNumbers(read_data):
+#     print("Iteration 16")
+#     print(f"Input file: {read_data}")
+
+#     with open(read_data, 'r') as file_list:
+#         file_ids = [line.strip().rstrip('.txt') for line in file_list]
+
+#     os.makedirs('/mnt/vol/multiplication', exist_ok=True)
+
+#     for file_id in file_ids:
+#         input_file_path = f'/mnt/vol/raw-data/{file_id}.txt'
+#         output_path = f'/mnt/vol/multiplication/{file_id}.txt'
+
+#         print(f"Processing file: {file_id}")
+#         print(f"Reading data from: {input_file_path}")
+#         with open(input_file_path, 'r') as input_file:
+#             number = int(input_file.read())
+
+#         doubled_number = number * 2
+
+#         print(f"Doubled number is {doubled_number}")
+#         print(f"Saving doubled number to: {output_path}")
+#         with open(output_path, 'w') as output_file:
+#             output_file.write(str(doubled_number))
+
+#         print(f"Processed file '{file_id}.txt' from {input_file_path}. Doubled the number from {number} to {doubled_number}. Saved to {output_path} as {output_file}")
+#         print()
+
+
+# doubleNumbers(sys.argv[1])
+
+# print("Finished step 2")
+
+
+
 import os
 import sys
+import json
 
 def doubleNumbers(read_data):
-    print("Iteration 16")
-    print(f"Input file: {read_data}")
+    input_number = int(sys.argv[1])
+    result = input_number * 2
+    print(result)
 
-    with open(read_data, 'r') as file_list:
-        file_ids = [line.strip().rstrip('.txt') for line in file_list]
 
-    os.makedirs('/mnt/vol/multiplication', exist_ok=True)
-
-    for file_id in file_ids:
-        input_file_path = f'/mnt/vol/raw-data/{file_id}.txt'
-        output_path = f'/mnt/vol/multiplication/{file_id}.txt'
-
-        print(f"Processing file: {file_id}")
-        print(f"Reading data from: {input_file_path}")
-        with open(input_file_path, 'r') as input_file:
-            number = int(input_file.read())
-
-        doubled_number = number * 2
-
-        print(f"Doubled number is {doubled_number}")
-        print(f"Saving doubled number to: {output_path}")
-        with open(output_path, 'w') as output_file:
-            output_file.write(str(doubled_number))
-
-        print(f"Processed file '{file_id}.txt' from {input_file_path}. Doubled the number from {number} to {doubled_number}. Saved to {output_path} as {output_file}")
-        print()
 
 
 doubleNumbers(sys.argv[1])
 
-print("Finished step 2")
+# print("Finished step 2")
